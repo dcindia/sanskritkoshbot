f2f267f86b199336801d8ebaabc793f51abc5b42
Darpan Verma <darpan5552@gmail.com>
2021-10-30 18:56:39 +0530

    Remove all html tags from answer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from html.parser import HTMLParser
+from io import StringIO
 import urllib.request
 import urllib.parse
 import re
@@ -13,10 +15,29 @@ logger = logging.getLogger(__name__)
 RAW_URL = "http://kosha.sanskrit.today/word/"
 
 
+class HTMLStripper(HTMLParser):
+    def __init__(self, *, convert_charrefs: bool = ...) -> None:
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, data: str) -> None:
+        self.text.write(data)
+        return super().handle_data(data)
+
+    def strip(self, html_text):
+        html_text = str.strip(html_text)
+        self.feed(html_text)
+        return self.text.getvalue()
+
+
 def meaning(word):
     transformed_word = urllib.parse.quote(word)
     url = RAW_URL + transformed_word
 
+    print("**************************************************************************")
     print(url)
     headers = {"User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
     request = urllib.request.Request(url, headers=headers)
@@ -24,7 +45,6 @@ def meaning(word):
     result = instance.read().decode('utf-8')
     file = open(r"result.html", 'w')
     file.write(urllib.parse.unquote(str(result)))
-    print("**************************************************************************")
     extracted_parts = re.findall(r'<h5>.*?</h5>.*?div class="card-body".*?</div>', str(result), re.DOTALL)
 
     for part in extracted_parts:
@@ -35,14 +55,15 @@ def meaning(word):
             answer_inside = re.search(r'<table>.*?<tr>(.*?)</tr>.*?</table>', part)
             answer_row = re.sub(r'<span.*?>|</span>', '', str(answer_inside.group(1)))
             answer_row = re.findall(r'<td>(.*?)</td>', answer_row)
-            answer_list = ["* " + k.strip() + "\n" for k in answer_row if k != '']
+            answer_list = ["* " + HTMLStripper().strip(k) + "\n" for k in answer_row if k != '']
 
             answer_string = ''.join(answer_list)
             return answer_string
 
         elif service.group(1) == "Hindi":
             answer_inside = re.search(r'<p class="card-text">(.*?)</p>', part)
-            answer_table = f'* {word}\n* {answer_inside.group(1)}'
+            answer = str(answer_inside.group(1))
+            answer_table = f'* {word}\n* {HTMLStripper().strip(answer)}'
             print(answer_table)
             return answer_table
 
